7c92046ae473ebd4c922b91521a56ac085dea753
Patryk Ławski <patryk.lawski@gmail.com>
2016-05-05 23:29:35 +0200

    Fib generator added

--- HelloJava/HelloJava.java
+++ HelloJava/HelloJava.java
@@ -1,5 +1,10 @@
 public class HelloJava{
+
 	public static void main(String[] args){
 		System.out.println("Hello Java!");
+		FibonGenerator fg = new FibonGenerator();
+		System.out.println(fg.number(3));
+		System.out.println(fg.number(6));
+		System.out.println(fg.number(10));
 	}
 }
